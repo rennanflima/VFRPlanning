058c3ca0498ed629de57c74aa111b8ee09051e04
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2017-10-16 14:26:40 -0500

    Atualizado

---
+++ src/main/java/br/rennan/goldplanning/controller/CadastroAerodromoBean.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rennan.goldplanning.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.rennan.goldplanning.model.Aerodromo;
+import br.rennan.goldplanning.service.CadastroAerodromoService;
+import br.rennan.goldplanning.service.NegocioException;
+import br.rennan.goldplanning.util.jsf.FacesUtil;
+
+/**
+ *
+ * @author rennan.lima
+ */
+@Named
+@ViewScoped
+public class CadastroAerodromoBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private CadastroAerodromoService cadastroAerodromoService;
+
+    private Aerodromo aerodromo;
+
+    public CadastroAerodromoBean() {
+        limpar();
+    }
+
+    public void inicializar() {
+        if (this.aerodromo == null) {
+            limpar();
+        }
+    }
+
+    public Aerodromo getAerodromo() {
+        return aerodromo;
+    }
+
+    public void setAerodromo(Aerodromo aerodromo) {
+        this.aerodromo = aerodromo;
+    }
+
+    private void limpar() {
+        this.aerodromo = new Aerodromo();
+    }
+
+    public void salvar() {
+        try {
+            this.aerodromo = cadastroAerodromoService.salvar(aerodromo);
+            limpar();
+
+            FacesUtil.addInfoMessage("Aerodromo salvo com sucesso!");
+        } catch (NegocioException ne) {
+            FacesUtil.addErrorMessage(ne.getMessage());
+        }
+    }
+
+    public boolean isEditando() {
+        return this.aerodromo.getId() != null;
+    }
+
+}
---
+++ src/main/java/br/rennan/goldplanning/model/Trecho.java
@@ -0,0 +1,213 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rennan.goldplanning.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author rennan.lima
+ */
+@Entity
+public class Trecho implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    @Column(nullable = false)
+    private String origem;
+    @NotNull
+    @Column(nullable = false)
+    private String destino;
+    @NotNull
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal distancia;
+    @NotNull
+    @Column(name = "rumo_magnetico", nullable = false)
+    private Integer rumoMagnetico;
+    @NotNull
+    @Column(name = "velocidade_indicada", nullable = false)
+    private Integer velocidadeIndicada;
+    @NotNull
+    @Column(nullable = false)
+    private String altitude;
+    @Column(name = "direcao_vento_voo")
+    private Integer direcaoVentoVoo;
+    @Column(name = "velocidade_vento_voo")
+    private Integer velocidadeVentoVoo;
+    @Column(name = "velocidade_solo")
+    private Integer velocidadeSolo;
+    @Column(name = "duracao_estimada_trecho", nullable = false)
+    private LocalTime duracaoEstimadaTrecho;
+    @Column(name = "hora_estimada_sobrevoo")
+    private LocalTime horaEstimadaSobrevoo;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal combustivel = BigDecimal.ZERO;
+    @Column(columnDefinition = "text")
+    private String observacao;
+    @ManyToOne
+    @JoinColumn(name = "plano_de_voo_id", nullable = false)
+    private PlanoDeVoo planoDeVoo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getTrecho() {
+        return destino;
+    }
+
+    public void setTrecho(String destino) {
+        this.destino = destino;
+    }
+
+    public BigDecimal getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(BigDecimal distancia) {
+        this.distancia = distancia;
+    }
+
+    public Integer getRumoMagnetico() {
+        return rumoMagnetico;
+    }
+
+    public void setRumoMagnetico(Integer rumoMagnetico) {
+        this.rumoMagnetico = rumoMagnetico;
+    }
+
+    public Integer getVelocidadeIndicada() {
+        return velocidadeIndicada;
+    }
+
+    public void setVelocidadeIndicada(Integer velocidadeIndicada) {
+        this.velocidadeIndicada = velocidadeIndicada;
+    }
+
+    public String getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
+    }
+
+    public Integer getDirecaoVentoVoo() {
+        return direcaoVentoVoo;
+    }
+
+    public void setDirecaoVentoVoo(Integer direcaoVentoVoo) {
+        this.direcaoVentoVoo = direcaoVentoVoo;
+    }
+
+    public Integer getVelocidadeVentoVoo() {
+        return velocidadeVentoVoo;
+    }
+
+    public void setVelocidadeVentoVoo(Integer velocidadeVentoVoo) {
+        this.velocidadeVentoVoo = velocidadeVentoVoo;
+    }
+
+    public Integer getVelocidadeSolo() {
+        return velocidadeSolo;
+    }
+
+    public void setVelocidadeSolo(Integer velocidadeSolo) {
+        this.velocidadeSolo = velocidadeSolo;
+    }
+
+    public LocalTime getDuracaoEstimadaTrecho() {
+        return duracaoEstimadaTrecho;
+    }
+
+    public void setDuracaoEstimadaTrecho(LocalTime duracaoEstimadaTrecho) {
+        this.duracaoEstimadaTrecho = duracaoEstimadaTrecho;
+    }
+
+    public LocalTime getHoraEstimadaSobrevoo() {
+        return horaEstimadaSobrevoo;
+    }
+
+    public void setHoraEstimadaSobrevoo(LocalTime horaEstimadaSobrevoo) {
+        this.horaEstimadaSobrevoo = horaEstimadaSobrevoo;
+    }
+
+    public BigDecimal getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(BigDecimal combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public PlanoDeVoo getPlanoDeVoo() {
+        return planoDeVoo;
+    }
+
+    public void setPlanoDeVoo(PlanoDeVoo planoDeVoo) {
+        this.planoDeVoo = planoDeVoo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Trecho other = (Trecho) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
